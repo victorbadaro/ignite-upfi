2602cd7f4cd22996c312b6ae9c19079c25a93eb0
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2024-11-28 02:14:41 -0300

    feat: add separated function to fetch images

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,27 @@ import { Header } from '../components/Header';
 import { Loading } from '../components/Loading';
 import { api } from '../services/api';
 
+interface Image {
+  title: string;
+  description: string;
+  url: string;
+  ts: number;
+  id: string;
+}
+
 export default function Home(): JSX.Element {
+  async function fetchImages({
+    pageParam = null,
+  }): Promise<{ after: string; data: Image[] }> {
+    const response = await api.get('http://localhost:3000/api/images', {
+      params: {
+        after: pageParam,
+      },
+    });
+
+    return response.data;
+  }
+
   const {
     data,
     isLoading,
@@ -16,21 +36,9 @@ export default function Home(): JSX.Element {
     isFetchingNextPage,
     fetchNextPage,
     hasNextPage,
-  } = useInfiniteQuery(
-    'images',
-    async ({ pageParam = null }) => {
-      const response = await api.get('http://localhost:3000/api/images', {
-        params: {
-          after: pageParam,
-        },
-      });
-
-      return response.data;
-    },
-    {
-      getNextPageParam: lastPage => (lastPage.after ? lastPage.after : null),
-    }
-  );
+  } = useInfiniteQuery('images', fetchImages, {
+    getNextPageParam: lastPage => (lastPage.after ? lastPage.after : null),
+  });
 
   const formattedData = useMemo(() => {
     return data?.pages.map(page => page.data).flat();
